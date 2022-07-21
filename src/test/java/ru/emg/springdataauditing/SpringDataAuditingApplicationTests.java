4aa50bd8aa77180135917ff3c88058286304f2f6
miegogen <usualgold@gmail.com>
2022-07-22 01:02:20 +0700

    maven downloaded dependenceies

---
+++ src/test/java/ru/emg/springdataauditing/SpringDataAuditingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.emg.springdataauditing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAuditingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
